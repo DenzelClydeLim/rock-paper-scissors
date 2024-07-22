2976eac8f1b803653c0bca88a534755136a2e2a5
Denzel Clyde Lim <dcflim2020@plm.edu.ph>
2024-07-23 00:12:11 +0800

    Programmed as per TOP's instructions
    Need further optimizations and customizations for improvement.

--- script.js
+++ script.js
@@ -1,76 +1,116 @@
+const playMoveContainer = document.querySelector("#playSectMoveContainer");
+const playMoveDesc = document.querySelector("#playSectTitle");
+const oppMoveDesc = document.querySelector("#oppSectMove");
+const scoreDesc = document.querySelector("#oppSectScore");
+const roundDesc = document.querySelector("#oppSectMatchNum");
+const oppImg = document.querySelector("#oppSectImg");
+const gameTitle = document.querySelector("#navBarLeft");
+let playerMove = "";
+let oppMove = "";
+let playerScore = 0;
+let oppScore = 0;
+let outcome = "";
+let seenWon = false;
+
+const restartBtn = document.createElement("button");
+restartBtn.textContent = "Restart the game";
+restartBtn.style.border = "1px solid black";
+restartBtn.className = "navBarRestartBtn";
+restartBtn.style.display = "none";
+restartBtn.addEventListener("click", () => {
+    playerScore = 0;
+    oppScore = 0;
+    playMoveDesc.textContent = "Time to choose your fighter!";
+    oppMoveDesc.textContent = "Neeko  is waiting for your move..."
+    roundDesc.textContent = "All is at peace.";
+    scoreDesc.textContent = `Player: ${playerScore} | Neeko: ${oppScore}`;
+    oppImg.src = "./images/cat-bored-pfp.jpg";
+    seenWon = false;
+    restartBtn.style.display = "none";
+})
+
+gameTitle.appendChild(restartBtn);
+playMoveContainer.addEventListener("click", (e) => {
+    switch (e.target.id) {
+        case "playSectRockBtn":
+            console.log(1);
+            playerMove = "rock";
+            break;
+        case "playSectPaperBtn":
+            playerMove = "paper";
+            break;
+        case "playSectScissorsBtn":
+            playerMove = "scissors";
+            break;
+    }
+    getComputerChoice();
+    playRound();
+})
+
+
 function getComputerChoice() {
-    let randomNum = Math.floor(Math.random() * 3 + 1);
-    let move;
-    if (randomNum === 1) {
-        move = "rock";
-    } else if (randomNum === 2) {
-        move = "paper";
-    } else {
-        move = "scissors";
+    const num = Math.floor(Math.random() * 3);
+    switch (num) {
+        case 0:
+            oppMove = "rock";
+            break;
+        case 1:
+            oppMove = "paper";
+            break;
+        case 2:
+            oppMove = "scissors";
+            break;
     }
-    (move);
-    console.log(`Opponent picked: ${move.charAt(0).toUpperCase() + move.slice(1)}`);
-    return move;
 }
 
-function getPlayerChoice() {
-    let choice;
-    while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
-        choice = prompt("Rock, paper or scissors?").toLowerCase();
-    }
-    console.log(`You picked: ${choice.charAt(0).toUpperCase() + choice.slice(1)}`);
-    return choice;
-}
-
-function playRound(playerMove, computerMove) {
+function playRound() {
+    playMoveDesc.textContent = `You chose ${playerMove}!`;
+    oppMoveDesc.textContent = `Neeko chose ${oppMove}`;
     if (playerMove === "rock") {
-        if (computerMove === "scissors") {
-            return "You won!";
-        } else if (computerMove === "paper") {
-            return "You lost!";
+        if (oppMove === "rock") {
+            outcome = "draw";
+        } else if (oppMove === "paper") {
+            outcome = "lose";
         } else {
-            return "Tie! No point gained!";
+            outcome = "win";
         }
     } else if (playerMove === "paper") {
-        if (computerMove === "rock") {
-            return "You won!";
-        } else if (computerMove === "scissors") {
-            return "You lost!";
+        if (oppMove === "paper") {
+            outcome = "draw";
+        } else if (oppMove === "scissors") {
+            outcome = "lose";
         } else {
-            return "Tie! No point gained!";
+            outcome = "win";
         }
     } else if (playerMove === "scissors") {
-        if (computerMove === "paper") {
-            return "You won!";
-        } else if (computerMove === "rock") {
-            return "You lost!";
+        if (oppMove === "scissors") {
+            outcome = "draw";
+        } else if (oppMove === "rock") {
+            outcome = "lose";
         } else {
-            return "Tie! No point gained!";
+            outcome = "win";
         }
     }
-}
+    if (outcome === "draw") {
+        roundDesc.textContent = "Match Result: Draw!";
+        oppImg.src = "./images/cat-bored-pfp.jpg";
+    } else if (outcome === "lose") {
+        roundDesc.textContent = "Match Result: Opponent Won!";
+        scoreDesc.textContent = `Player: ${playerScore} | Neeko: ${++oppScore}`;
+        oppImg.src = "./images/cat-celebrate-pfp.jpg";
+    } else if (outcome === "win") {
+        roundDesc.textContent = "Match Result: You Won!";
+        scoreDesc.textContent = `Player: ${++playerScore} | Neeko: ${oppScore}`;
+        oppImg.src = "./images/cat-sad-pfp.jpg";
+    }
 
-function playGame() {
-    let i = 0;
-    let result;
-    let playerScore = 0;
-    let computerScore = 0;
-    while (i < 5) {
-        result = playRound(getPlayerChoice(), getComputerChoice());
-        console.log(result);
-        result === "You won!" ? ++playerScore : result === "You lost!" ? ++computerScore : null;
-        console.log(`Player Score: ${playerScore}`);
-        console.log(`Computer Score: ${computerScore}`);
-        i++;
-    }
-    if (playerScore > computerScore) {
-        return "You are the champion!";
-    } else if (computerScore > playerScore) {
-        return "Computer wins!";
-    } else if (playerScore === computerScore) {
-        return "It's a tie! You're both winners";
-    } else {
-        return "Woah! Nobody scored a point.";
+    if (playerScore === 5 || oppScore === 5) {
+        if (playerScore === 5 && !seenWon) {
+            roundDesc.textContent += ` With a score of 5, you have won the game!`;
+        } else if (oppScore === 5 && !seenWon) {
+            roundDesc.textContent += " In an unfortunate turn of events, Neeko has won the game.";
+        }
+        seenWon = true;
+        restartBtn.style.display = "inline";
     }
 }
-console.log(playGame());
